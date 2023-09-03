977de3d8bdfd91c060aea970672c4af7a668265c
ValeryArauco <141700272+ValeryArauco@users.noreply.github.com>
2023-09-02 20:57:43 -0400

    Implementa obtenerPosicionInicial para devolver la posición inicial ingresada por el usuario

--- src/controladorAuto.test.js
+++ src/controladorAuto.test.js
@@ -3,10 +3,17 @@ import ControladorAuto from "./controladorAuto.js";
 const constroladorAuto = new ControladorAuto();
 
 describe("Permitir que el usuario ingrese la posición inicial del auto y mostrarla", () => {
-  it("deberia devolver la posición inicial ingresada por el usuario", () => {
+  it("Deberia devolver la posición inicial ingresada por el usuario", () => {
     
     const comando = "5,5/1,2N/IAIAIAIAA";
     const posicionInicial = constroladorAuto.obtenerPosicionInicial(comando);
     expect(posicionInicial).toEqual("1,2N");
+  });
+
+  it("Deberia devolver cualquier posición inicial ingresada por el usuario", () => {
+    
+    const comando = "5,5/2,2N/IAIAIAIAA";
+    const posicionInicial = constroladorAuto.obtenerPosicionInicial(comando);
+    expect(posicionInicial).toEqual("2,2N");
   });
 });
